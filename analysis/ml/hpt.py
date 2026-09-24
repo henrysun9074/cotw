f160bd4503bc9613de64405bb956128f0a52916e
henrysun9074 <hs325@duke.edu>
2026-09-24 18:31:59 -0400

    update ML code + model outputs

--- analysis/ml/hpt.py
+++ analysis/ml/hpt.py
@@ -29,9 +29,6 @@ y = coral_df['scleractinia']
 X = X.to_numpy()
 y = y.to_numpy()
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=123, stratify=y_train)
 
